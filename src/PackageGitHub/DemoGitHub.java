15806165a3de55edaa32ffabf5fdee1ef0a9c605
rusol <rusol@192.168.1.191>
2023-07-29 11:35:59 -0400

    Add method

--- src/PackageGitHub/DemoGitHub.java
+++ src/PackageGitHub/DemoGitHub.java
@@ -3,6 +3,12 @@ package PackageGitHub;
 public class DemoGitHub {
 	public static void main(String[] args) {
 		System.out.println("zoom");
+	
+	}
+	//add method
+	public static void add(int a , int b ) {
+		System.out.println(a+b);
+		
 	}
 
 }
